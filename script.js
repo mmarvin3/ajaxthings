7080d1ae7347342c907a58fbdcaf7c0a8227036b
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-05-05 16:47:37 -0400

    work in progress: we can perform ajax

---
+++ script.js
@@ -0,0 +1,21 @@
+// alert("It works")
+
+//How to request data from an API using javaScript and jQuery
+
+//There are many ways to do this!
+//1. jQuerys $.ajax() function
+//2. The Browser's fetch() function
+//3. Using the Axios http client library
+
+const URL = 'http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard';
+
+function handleGetData() {
+    $.ajax(URL).then(function (data) {
+        console.log('movie data is ready')
+        console.log(data)
+    }), function (error) {
+        console.log('something is wrong')
+        console.log(error)
+    }
+
+}
