5f30ef0a06d5a664bda9d694481c11ebb2dc5a70
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-05-06 16:14:07 -0400

    Ajax warm up

--- script.js
+++ script.js
@@ -7,15 +7,37 @@
 //2. The Browser's fetch() function
 //3. Using the Axios http client library
 
-const URL = 'http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard';
 
-function handleGetData() {
-    $.ajax(URL).then(function (data) {
+//VARIABLES
+const URL = 'http://www.omdbapi.com/?apikey=a71b148a&t=';
+
+//ELEMENT REFERENCES
+const $title = $('#title');
+const $year = $('#year');
+const $rating = $('#rated');
+const $form = $('form');
+const $input = $(`input[type="text"]`);
+
+//EVENT LISTENERS
+$form.on('submit', handleGetData)
+
+//FUNCTIONS
+
+function handleGetData(event) {
+
+    event.preventDefault()
+    const userInput = $input.val();
+
+    $.ajax(URL + userInput).then(function (data) {
         console.log('movie data is ready')
         console.log(data)
-    }), function (error) {
+        $title.text(data.Title)
+        $year.text(data.Year)
+        $rating.text(data.Rated)
+        $('main').append(`<img src="${data.Poster}"/>`)
+    }, function (error) {
         console.log('something is wrong')
         console.log(error)
-    }
+    })
 
 }
